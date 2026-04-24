4ce7dd79f14050a83eb5dc3d6701b68fdbf7208a
Ваше Имя <you@example.com>
2026-04-24 13:16:00 +0300

    13:15

--- agent/run.bat
+++ agent/run.bat
@@ -1,4 +1,3 @@
 @echo off
-REM Стартовый режим можно задать, но с сервера он всё равно переопределится.
-agent.exe --server=192.168.2.222 --port=8000 --id=%COMPUTERNAME% ^
-          --codec=mjpeg --fps=30 --quality=4 --bitrate=4M
+REM Для HTTP (напрямую к FastAPI на порту 8000)
+agent.exe --server=dev.local --port=8000 --https=false --id=%COMPUTERNAME% --codec=mjpeg --fps=30 --quality=4 --bitrate=4M
--- agent/runssl.bat
+++ agent/runssl.bat
@@ -1,6 +1,3 @@
 @echo off
-REM Для HTTPS добавьте --https=true
-REM Для HTTP (разработка) --https=false
-REM Подключаемся через nginx (порт 443), а не напрямую к FastAPI (8000)
-agent.exe --server=dev.local --port=443 --https=true --id=%COMPUTERNAME% ^
-          --codec=mjpeg --fps=30 --quality=4 --bitrate=4M
+REM Для HTTPS (через nginx на порту 443)
+agent.exe --server=dev.local --port=443 --https=true --id=%COMPUTERNAME% --codec=mjpeg --fps=30 --quality=4 --bitrate=4M
